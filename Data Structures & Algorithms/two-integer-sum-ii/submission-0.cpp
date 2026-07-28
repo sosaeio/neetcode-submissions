bad36fd43426beca10e27d552f130f5b6b8a64c3
Esosa E <ekhoragbonesosa1@gmail.com>
2026-07-27 18:07:31 -0700

    Bulk sync: 4 submissions

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+      int l = 0, r = numbers.size() - 1;
+      while(l <= r){
+        int sum = numbers[l] + numbers[r];
+        if(sum < target){
+          l++;
+          continue;
+        }else if(sum > target){
+          r--;
+          continue;
+        }else{
+          return {l + 1, r + 1};
+        }
+      }
+      return {-1 , -1};
+    }
+};
