4daa4991b3b846e4551ca9db0a3d4e1ceebbf735
Esosa E <ekhoragbonesosa1@gmail.com>
2026-08-27 17:25:13 -0700

    Add: remove-element - submission-1

---
+++ Data Structures & Algorithms/remove-element/submission-1.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int k = 0;
+        int l = 0;
+        int r = nums.size();
+        while(l < r){
+            if(nums[l] == val){
+                nums[l] = nums[r - 1];
+                r--;
+            }else l++;
+        }
+        return l;
+    }
+};
+/*
+inx [0,1,2,3,4,5,6,7]
+num [0,1,2,2,3,0,4,2]
+
+val = 2
+0,1,2,2,3,0,4,2
+^             ^ 
+l             r no change for left
+0,1,2,2,3,0,4,2
+  l           r no change for left
+0,1,4,2,3,0,2,2
+    l       r  now move r down so left can take this value
+0,1,4,0,3,2,2,2
+      l   r    now move r down so left can take this value
+0,1,4,0,3,2,2,2
+        l r    moves are complete
+*/
