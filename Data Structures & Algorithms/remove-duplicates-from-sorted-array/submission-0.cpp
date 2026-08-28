7a3f4691b331946866e43c1188873ee313b982b3
Esosa E <ekhoragbonesosa1@gmail.com>
2026-08-28 12:13:02 -0700

    Add: remove-duplicates-from-sorted-array - submission-0

---
+++ Data Structures & Algorithms/remove-duplicates-from-sorted-array/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int write = 0;
+        for(int read = 1; read < nums.size(); ++read){
+            if(nums[read] != nums[write]){
+                write++;
+                nums[write] = nums[read];
+            }
+        }
+        return write + 1;
+    }
+};
+/*
+read is fast pointer
+write is slow pointer
+[1,1,2,3,4]
+ s f        nums[f] != nums[s] fails so f moves and s stays
+[1,1,2,3,4]
+ s   f      nums[f] != passes so 
+
+*/
